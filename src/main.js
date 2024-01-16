b60ad1289ac30573414309f721072ecf754246b5
Ethan Ramos <ethanramos@ethans-mbp-7.myfiosgateway.com>
2024-01-15 23:06:36 -0500

    Revamp styling to landing page

--- src/main.js
+++ src/main.js
@@ -1,32 +1,30 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  let remainingTime = 30;
-  let isStopped = true;
-  const timerElement = document.getElementById("gameTimer");
-  const startButton = document.getElementById("startGameButton");
-  let timer;
+let remainingTime = 30;
+let isStopped = true;
+const timerElement = document.getElementById("gameTimer");
+const startButton = document.getElementById("startGameButton");
+let timer;
 
-  const displayTimer = () => {
-    timerElement.style.color = "black"; // reset color
-    remainingTime--;
+const displayTimer = () => {
+  timerElement.style.color = "black"; // reset color
+  remainingTime--;
+  timerElement.innerHTML = remainingTime;
+  if (remainingTime <= 5 && remainingTime > 0) {
+    timerElement.style.color = "red";
+  }
+  if (remainingTime === 0) {
+    clearInterval(timer);
+    isStopped = true;
+    remainingTime = 30;
+  }
+};
+
+const startTimer = () => {
+  console.log("startTimer");
+  if (isStopped) {
+    timer = setInterval(displayTimer, 1000);
+    isStopped = false;
     timerElement.innerHTML = remainingTime;
-    if (remainingTime <= 5 && remainingTime > 0) {
-      timerElement.style.color = "red";
-    }
-    if (remainingTime === 0) {
-      clearInterval(timer);
-      isStopped = true;
-      remainingTime = 30;
-    }
-  };
-
-  const startTimer = () => {
-    console.log("startTimer");
-    if (isStopped) {
-      timer = setInterval(displayTimer, 1000);
-      isStopped = false;
-      timerElement.innerHTML = remainingTime;
-      console.log(remainingTime);
-    }
-  };
-  startButton.onclick = startTimer;
-});
+    console.log(remainingTime);
+  }
+};
+startButton.onclick = startTimer;
