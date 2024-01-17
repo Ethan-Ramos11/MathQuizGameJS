a04fb0a608e7591d9b6f33fb2af6957087db7930
Ethan Ramos <ethanramos@ethans-mbp-7.myfiosgateway.com>
2024-01-16 22:56:10 -0500

    Added functions to generate a pair of numbers for the question and a function to check if two numbers are divisible

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+const validOperands = ["+", "-", "*", "/"];
 let remainingTime = 30;
 let isStopped = true;
 const timerElement = document.getElementById("gameTimer");
@@ -11,6 +12,15 @@ const displayTimer = () => {
   if (remainingTime <= 5 && remainingTime > 0) {
     timerElement.style.color = "red";
   }
+};
+
+const startTimer = () => {
+  if (isStopped) {
+    timer = setInterval(displayTimer, 1000);
+    isStopped = false;
+    timerElement.innerHTML = remainingTime;
+    console.log(remainingTime);
+  }
   if (remainingTime === 0) {
     clearInterval(timer);
     isStopped = true;
@@ -18,13 +28,28 @@ const displayTimer = () => {
   }
 };
 
-const startTimer = () => {
-  console.log("startTimer");
-  if (isStopped) {
-    timer = setInterval(displayTimer, 1000);
-    isStopped = false;
-    timerElement.innerHTML = remainingTime;
-    console.log(remainingTime);
-  }
+const isDivisible = (numOne, numTwo) => {
+  return numTwo !== 0 && numOne % numTwo === 0;
 };
-startButton.onclick = startTimer;
+
+const generatePair = (operand) => {
+  if (operand === undefined || !validOperands.includes(operand)) {
+    throw new Error("Invalid operand");
+  }
+  let firstNumber = Math.floor(Math.random() * 100) + 1;
+  let secondNumber = Math.floor(Math.random() * 100) + 1;
+  if (operand === "/") {
+    while (!isDivisible(firstNumber, secondNumber)) {
+      firstNumber = Math.floor(Math.random() * 100) + 1;
+      secondNumber = Math.floor(Math.random() * 100) + 1;
+    }
+  }
+  const pair = [firstNumber, secondNumber];
+  return pair;
+};
+
+startButton.addEventListener("click", () => {
+  startTimer();
+  generatePair("/");
+});
+console.log(generatePair("/"));
