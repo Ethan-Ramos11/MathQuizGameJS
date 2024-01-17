1f46a9d30e9e41bf153b109ef62ea4eb1eb269c2
Ethan Ramos <ethanramos@ethans-mbp-7.myfiosgateway.com>
2024-01-17 00:01:29 -0500

    Added two functions, one to generate the prompt and one to display it

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ let remainingTime = 30;
 let isStopped = true;
 const timerElement = document.getElementById("gameTimer");
 const startButton = document.getElementById("startGameButton");
+const promptElement = document.getElementById("question");
+const questionAndAnswer = document.getElementById("questionAndAnswer");
 let timer;
 
 const displayTimer = () => {
@@ -12,6 +14,12 @@ const displayTimer = () => {
   if (remainingTime <= 5 && remainingTime > 0) {
     timerElement.style.color = "red";
   }
+  if (remainingTime === 0) {
+    clearInterval(timer);
+    questionAndAnswer.style.display = "none";
+    isStopped = true;
+    remainingTime = 30;
+  }
 };
 
 const startTimer = () => {
@@ -19,12 +27,6 @@ const startTimer = () => {
     timer = setInterval(displayTimer, 1000);
     isStopped = false;
     timerElement.innerHTML = remainingTime;
-    console.log(remainingTime);
-  }
-  if (remainingTime === 0) {
-    clearInterval(timer);
-    isStopped = true;
-    remainingTime = 30;
   }
 };
 
@@ -36,20 +38,50 @@ const generatePair = (operand) => {
   if (operand === undefined || !validOperands.includes(operand)) {
     throw new Error("Invalid operand");
   }
-  let firstNumber = Math.floor(Math.random() * 100) + 1;
-  let secondNumber = Math.floor(Math.random() * 100) + 1;
+  let firstNumber = Math.floor(Math.random() * 10) + 1;
+  let secondNumber = Math.floor(Math.random() * 10) + 1;
   if (operand === "/") {
     while (!isDivisible(firstNumber, secondNumber)) {
-      firstNumber = Math.floor(Math.random() * 100) + 1;
-      secondNumber = Math.floor(Math.random() * 100) + 1;
+      firstNumber = Math.floor(Math.random() * 144) + 1;
+      secondNumber = Math.floor(Math.random() * 12) + 1;
     }
   }
   const pair = [firstNumber, secondNumber];
   return pair;
 };
 
+const generatePrompt = () => {
+  let prompt = "";
+  const operand = validOperands[Math.floor(Math.random() * 4)];
+  let pair = generatePair(operand);
+  let answer = 0;
+  if (operand === "+") {
+    answer = pair[0] + pair[1];
+    prompt = `What is ${pair[0]} + ${pair[1]}?`;
+  } else if (operand === "-") {
+    answer = pair[0] - pair[1];
+    prompt = `What is ${pair[0]} - ${pair[1]}?`;
+  } else if (operand === "*") {
+    answer = pair[0] * pair[1];
+    prompt = `What is ${pair[0]} * ${pair[1]}?`;
+  } else {
+    answer = pair[0] / pair[1];
+    prompt = `What is ${pair[0]} / ${pair[1]}?`;
+  }
+  return [prompt, answer];
+};
+const displayPrompt = () => {
+  const [prompt, answer] = generatePrompt();
+  promptElement.innerHTML = prompt;
+};
+
 startButton.addEventListener("click", () => {
+  if (questionAndAnswer.style.display === "none") {
+    questionAndAnswer.style.display = "block";
+  } else {
+    questionAndAnswer.style.display = "none";
+  }
   startTimer();
   generatePair("/");
+  displayPrompt();
 });
-console.log(generatePair("/"));
